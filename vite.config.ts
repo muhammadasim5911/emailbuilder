8243a3b0b9af1ca838f0b00c4427ee38f8529c6a
Asim <asimshams570@gmail.com>
2026-02-03 11:29:14 +0500

    fix package lock issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+import dts from 'vite-plugin-dts'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/components/EmailTemplateBuilder.tsx'),
